acb91e9f3d1fca4b3b8aa17bd460f2918da370e0
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-03-25 18:52:58 +0900

    Checking in changes prior to tagging of version 0.02.  Changelog diff is:
    diff --git a/Changes b/Changes
    index 54f2f5a..67d2dc5 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,4 +1,9 @@
     Revision history for Perl extension Test::SharedFork
    
    +0.02
    +
    +        - ooppppsss. Test::Fork was already used by schwern.
    +          rename to Test::SharedFork
    +
     0.01    Wed Mar 25 14:06:28 2009
             - original version

--- lib/Test/SharedFork.pm
+++ lib/Test/SharedFork.pm
@@ -1,7 +1,7 @@
 package Test::SharedFork;
 use strict;
 use warnings;
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 use Test::Builder;
 use Test::SharedFork::Scalar;
 use Test::SharedFork::Array;
