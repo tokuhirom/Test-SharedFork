a2f8906b6d2e1dc305fa96667d29eef4835cc455
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-03-26 11:53:02 +0900

    Checking in changes prior to tagging of version 0.03.  Changelog diff is:
    diff --git a/Changes b/Changes
    index 67d2dc5..9c23760 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,10 @@
     Revision history for Perl extension Test::SharedFork
    
    +0.03
    +
    +        - added Test::SharedFork->fork()
    +        - use flock(2) instead of IPC::ShareLite
    +
     0.02
    
             - ooppppsss. Test::Fork was already used by schwern.

--- lib/Test/SharedFork.pm
+++ lib/Test/SharedFork.pm
@@ -1,7 +1,7 @@
 package Test::SharedFork;
 use strict;
 use warnings;
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 use Test::Builder;
 use Test::SharedFork::Scalar;
 use Test::SharedFork::Array;
