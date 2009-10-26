76b2d91b4fc7d219ddf7119070deb5b38f60b8ef
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-10-26 16:24:06 +0900

    0.08

--- lib/Test/SharedFork.pm
+++ lib/Test/SharedFork.pm
@@ -2,7 +2,7 @@ package Test::SharedFork;
 use strict;
 use warnings;
 use base 'Test::Builder::Module';
-our $VERSION = '0.07_01';
+our $VERSION = '0.08';
 use Test::Builder 0.32; # 0.32 or later is needed
 use Test::SharedFork::Scalar;
 use Test::SharedFork::Array;
