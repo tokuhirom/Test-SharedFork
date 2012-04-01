1fc5cb9159c68e6dd6eb79d38250283368fa5890
geistteufel <geistteufel@yahoo.fr>
2012-04-01 14:47:25 +0200

    Properly fix for TB2 using event handlers
    Wrapping all the TB2 methods is slow and dangerous.  Instead...
    
    At the start of event processing...
    1. lock the history storage
    2. read in a stored history object
    3. Use that history object
    
    Process the event normally
    
    At the end of event processing...
    1. Write out the history object to the storage file
    2. Unlock the history storage
    
    This synchronizes not just the test number, but all events
    between processes, not matter if they're using Test::Builder
    or not.
    
    All but a few tests pass with TB2.

---
+++ lib/Test/SharedFork/Handler/Early.pm
@@ -0,0 +1,75 @@
+package Test::SharedFork::Handler::Early;
+
+use Storable qw/retrieve/;
+use TB2::Mouse;
+with "TB2::EventHandler";
+
+use Fcntl qw/:flock :seek :DEFAULT/;
+
+has filename => (
+    is  => 'ro',
+    isa => 'Str',
+);
+
+has fh => (
+    is  => 'rw',
+    isa => 'GlobRef'
+);
+
+has pid => (
+    is  => 'rw',
+    isa => 'Int',
+    default => $$
+);
+
+
+# handle_event() handles anything not handled by some other method
+sub handle_event  {
+    my $self = shift;
+    my($event, $ec) = @_;
+
+    my $history = $self->_read_history_file;
+    return unless $history;
+
+    # use the history we read
+    $ec->history($history);
+
+    # continue processing the event
+    return;
+}
+
+
+sub _read_history_file {
+    my $self = shift;
+
+    # If we forked since last opening the file, get a new
+    # file handle.  Children share file descriptors with their
+    # parent, and flock works on file descriptors.
+    if( !$self->fh || ($self->pid != $$) ) {
+        close $self->fh if $self->fh;
+        open my $fh, '<', $self->filename or die $!;
+        $self->fh($fh);
+        $self->pid($$);
+    }
+
+    flock $self->fh, LOCK_EX or die $!;
+
+    # read the history file
+    my $history = retrieve($self->filename) || die "Can't retrieve";
+
+    # ignore an empty history file
+    return if ! defined $history or ! length $history;
+
+    # ignore an empty hash
+    return if ref $history eq 'HASH' and ! keys %$history;
+
+    # error out if the history is corrupt
+    die "Corrupted TB2::History" unless eval { $history->isa("TB2::History") };
+
+    return $history;
+}
+
+
+ no TB2::Mouse;
+
+1;
---
+++ lib/Test/SharedFork/Handler/Late.pm
@@ -0,0 +1,32 @@
+package Test::SharedFork::Handler::Late;
+
+use Storable qw/nstore/;
+use TB2::Mouse;
+with "TB2::EventHandler";
+
+use Fcntl qw/:flock :seek/;
+
+has early_handler => (
+    is  => 'ro',
+    isa => 'Test::SharedFork::Handler::Early'
+);
+
+
+# handle_event() handles anything not handled by some other method
+sub handle_event  {
+    my $self = shift;
+    my($event, $ec) = @_;
+
+    # store the history
+    nstore($ec->history, $self->early_handler->filename);
+
+    # unlock
+    flock $self->early_handler->fh, LOCK_UN or die $!;
+
+    # continue processing the event
+    return;
+}
+
+no TB2::Mouse;
+
+1;
