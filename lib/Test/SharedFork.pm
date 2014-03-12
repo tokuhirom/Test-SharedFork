630dab614ad9c30377efa1a701caef84e121cbcb
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-03-13 05:13:42 +0900

    Checking in changes prior to tagging of version 0.23.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 2f1008c..4fcfd1e 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,11 @@ Revision history for Perl module Test::SharedFork
    
     {{$NEXT}}
    
    +0.23 2014-03-12T20:12:11Z
    +
    +    - 06_fail_lineno.t fails on Windows due to path separator #9
    +      (nanis)
    +
     0.22 2014-03-11T00:01:43Z
    
         - Care the is_passing value.

--- lib/Test/SharedFork.pm
+++ lib/Test/SharedFork.pm
@@ -2,7 +2,7 @@ package Test::SharedFork;
 use strict;
 use warnings;
 use base 'Test::Builder::Module';
-our $VERSION = '0.22';
+our $VERSION = '0.23';
 use Test::Builder 0.32; # 0.32 or later is needed
 use Test::SharedFork::Scalar;
 use Test::SharedFork::Array;
